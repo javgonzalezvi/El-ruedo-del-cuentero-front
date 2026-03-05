7560f0fe7714ae417f07f9854741112e0673320f
Javier González <92602146+javgonzalezvi@users.noreply.github.com>
2026-03-05 17:55:52 -0500

    Fix FormEvento

--- src/components/Cuentero/FormEvento.jsx
+++ src/components/Cuentero/FormEvento.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { eventosAPI, apiFetch } from "../../services/api";
-import styles from "./FormCuentero.module.css";
+import styles from "./Formcuentero.module.css";
 
 const CAMPOS_VACIOS = {
   titulo:        "",
